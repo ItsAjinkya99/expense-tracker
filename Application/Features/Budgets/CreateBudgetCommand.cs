1c62770d31550332f18e8859691aaa6a790969e5
Ajinkya <ajinkyapatharkar5194@gmail.com>
2026-09-19 15:43:32 +0530

    completed infrastructure layer

---
+++ Application/Features/Budgets/CreateBudgetCommand.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Domain.ValueObjects;
+using MediatR;
+
+namespace Application.Features.Budgets;
+
+// Payload (Command)
+public record CreateBudgetCommand(Guid UserId, Guid CategoryId, decimal Amount, string Currency, int Month, int Year) : IRequest<Guid>;
+
+
+// Behavior
+internal class CreateBudgetCommandHandler(IBudgetRepository budgetRepo) : IRequestHandler<CreateBudgetCommand, Guid>
+{
+    public async Task<Guid> Handle(CreateBudgetCommand request, CancellationToken cancellationToken)
+    {
+        var limit = Money.Create(request.Amount, request.Currency);
+        var budget = Budget.Create(request.UserId, request.CategoryId, limit, request.Month, request.Year);
+
+        await budgetRepo.AddAsync(budget, cancellationToken);
+        return budget.Id;
+    }
+}
+
